477bdc92c4a3c189cbe3efe42daff9dfd727008f
unknown <shaktiydml@gmail.com>
2023-08-13 10:50:17 +0530

    First program

---
+++ 1.py
@@ -0,0 +1,37 @@
+# comment
+# what is comments : line comments and block comments
+'''
+   Purpose:
+   Author:
+   DAte:
+'''
+print("Hello welcome to python programming!!!") # dflkajdlfjdlfjdfljfldjfl djlkfjdl
+
+# Varaible : 
+a=343 # integer value 
+
+'''
+   int a =34
+
+'''
+print("value of a ", a, type(a))
+b = "Hello Ram and Prakash"  # string 
+
+print("dkladjfalkdfjlkdsfj: ", b, type(b))
+
+
+a =34.4
+print("value of a ", a, type(a))
+print("")
+d
+a = input("Please input user value")
+print("value of a :",a , type(a))
+a = int(a)
+print("value of a :",a , type(a))
+
+# #include <stdio.h>
+# int main(int argc, char *argv)
+# {
+#     printf("hello welcome to c");
+#     return 0;
+# }
